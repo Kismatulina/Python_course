b8fd7b9323ec7dc05e84c3b59d6a6e90b20b0193
Lydmila Kismatulina <tenzinwangmo@yandex.ru>
2023-06-24 21:11:50 +0300

    записала домашнюю 8

--- homework7..py
+++ homework7..py
@@ -14,11 +14,13 @@
 # Вывод:
 # Парам пам-па
 
-vinny_words = input("Введите стихотворение: ")
-lett_count = lambda sentensis: sum([1 for letter in sentensis if letter.lower() in "аеёиоуыэюя"])
-phrases = vinny_words.split()
-vowels_in_phrases = [lett_count(phrase.replace("-", "")) for phrase in phrases]
-print('Парам пам-пам'if vowels_in_phrases.count(vowels_in_phrases[0]) == len(vowels_in_phrases) else 'Пам парам')
+vinny_words = input("Введите стихотворение: ") # 
+lett_count = lambda sentensis: sum([1 for letter in sentensis if letter.lower() in "аеёиоуыэюя"]) # определяем ф для подсчета гласных в предложении(sentensis) и letter.lower в нижний регистр ввели
+phrases = vinny_words.split() # сплит разделил по пробелам фразы 
+# print(vinny_words)
+vowels_in_phrases = [lett_count(phrase.replace("-", "")) for phrase in phrases] # идем по строке гласные буквы в фразе, подсчтитанных гласные lett_count заменяем тире на пусто и заводим новую функцию phrase  
+print('Парам пам-пам'if vowels_in_phrases.count(vowels_in_phrases[0]) == len(vowels_in_phrases) else 'Пам парам') # цикл сразу сравниваю значение гласных,
+# если кол-во гласных букв в фразе (хранятся индексы) равны длине гласных букв, выводим Парам пам-пам иначе Пам парам  
 
 # Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
 # которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и
@@ -63,9 +65,9 @@ print('Парам пам-пам'if vowels_in_phrases.count(vowels_in_phrases[0])
 # print_operation_table(lambda x, y: x*y)
 
 # 3 вариант
-def print_operation_table(operation, num_rows=6, num_columns=6):
-    table_list = [[operation(i, j) for i in range(1, num_rows + 1)] for j in range(1, num_columns + 1)]
-    for line in table_list:
-        print(*line)
+# def print_operation_table(operation, num_rows=6, num_columns=6):
+#     table_list = [[operation(i, j) for i in range(1, num_rows + 1)] for j in range(1, num_columns + 1)]
+#     for line in table_list:
+#         print(*line)
 
-print_operation_table(lambda x, y: x*y)
+# print_operation_table(lambda x, y: x*y)
--- seminar10.py
+++ seminar10.py
@@ -15,3 +15,48 @@ https://vk.com/@we_use_python-uchimsya-sozdavat-pakety-python
 Работа с крутой библиотекой Selenium. Очень полезный инструмент, позволяет работать с баузером (парасить, автоматизировать действия и т.д.): https://vk.com/wall-79831840_58984
 Крутая библиотека для работы с интерфейсами (PyQt): https://vk.com/wall-79831840_58959
 Нейронка, которая знакомиться с девушками в Тиндере XD: https://vk.com/wall-194576836_13051
+
+
+graph = sns.scatterplot(data=penguins.dropna(), x="species", y="island") #дропна убрать пропуски!!! Хью оттенок, сайс масса точки, вид показывает где живут , стайлс ,вместо острова поменять на вид пингвина
+С наилучшими пожеланиями!
+graph = sns.scatterplot(data=penguins.dropna(), x="bill_depth_mm", y="body_mass_g", hue = 'sex', size = 'body_mass_g', style = 'island')
+
+
+from sqlalchemy import true
+penguins = sns.load_dataset("penguins")
+penguins.dropna(inplace = True) # делает копию даты без пропусков (NaN)
+penguins.head()
+
+https://pythonru.com/biblioteki/seaborn-plot
+
+
+
+
+mport matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = '3d')
+
+x = penguins.iloc[:, 2]
+y = penguins.iloc[:, 3]
+z = penguins.iloc[:, 4]
+
+ax.set_xlabel("Длина клюва")
+ax.set_ylabel("Ширина клюва")
+ax.set_zlabel("Длина ласты")
+
+ax.scatter(x, y, z)
+
+
+
+plt.show()
+Добрый день!
+# PairGrid
+lst = ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g', 'sex']
+g = sns.PairGrid(penguins[lst])
+g.map(sns.scatterplot)
+С наилучшими пожеланиями!
+
+sns.heatmap(penguins.corr(), vmin=-1, vmax=1)
